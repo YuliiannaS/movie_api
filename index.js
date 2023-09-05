7c0a4c278ca9c4b2514e2e2a7e51bb63b13145d4
Yuliia <yulja01@gmail.com>
2023-09-05 12:02:44 -0500

    created database using Mongoose models

--- index.js
+++ index.js
@@ -1,107 +1,81 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const { Director, Genre, Movie, User } = require('./models');
 
 const app = express();
 const port = 8080;
+
+mongoose.connect('mongodb://127.0.0.1:27017/moviedb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
 
 // Middleware
 app.use(express.json()); // For parsing JSON data
 app.use(morgan('dev'));  // Using Morgan for request logging
 app.use(express.static('public')); // Serve static files
 
-let users = [
-    {
-        email: 'test@google.com',
-        username: 'testuser',
-        password: '123',
-        favorite: [],
-    },
-];
-
-const movies = [
-    {
-        title: 'The Shawshank Redemption',
-        description: 'Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.',
-        genre: {
-            name: 'Drama',
-            description: 'a type of story acted out before an audience',
-        },
-        director: {
-            name: 'Frank Darabont',
-            bio: 'Three-time Oscar nominee Frank Darabont was born in a refugee camp in 1959 in Montbeliard, France, the son of Hungarian parents who had fled Budapest during the failed 1956 Hungarian revolution.',
-            birthYear: '1959',
-        },
-        imageUrl: 'https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1200_.jpg',
-        featured: true,
-    },
-    {
-        title: 'Requiem for a Dream',
-        description: 'The drug-induced utopias of four Coney Island people are shattered when their addictions run deep.',
-        genre: {
-            name: 'Drama',
-            description: 'a type of story acted out before an audience',
-        },
-        director: {
-            name: 'Darren Aronofsky',
-            bio: 'Darren Aronofsky was born February 12, 1969, in Brooklyn, New York. Growing up, Darren was always artistic: he loved classic movies and, as a teenager, he even spent time doing graffiti art.',
-            birthYear: '1969',
-        },
-        imageUrl: 'https://m.media-amazon.com/images/M/MV5BOTdiNzJlOWUtNWMwNS00NmFlLWI0YTEtZmI3YjIzZWUyY2Y3XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_FMjpg_UY1920_.jpg',
-        featured: true,
-    },
-    {
-        title: ' Eternal Sunshine of the Spotless Mind',
-        description: 'When their relationship turns sour, a couple undergoes a medical procedure to have each other erased from their memories for ever.',
-        genre: {
-            name: 'Drama',
-            description: 'a type of story acted out before an audience',
-        },
-        director: {
-            name: 'Michel Gondry',
-            bio: 'He grew up in Versailles with a family who was very influenced by pop music. When he was young, Gondry wanted to be a painter or an inventor. In the 80s he entered in an art school in Paris where he could develop his graphic skills and where he also met friends with whom he created a pop-rock band called Oui-Oui.',
-            birthYear: '1963',
-        },
-        imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTY4NzcwODg3Nl5BMl5BanBnXkFtZTcwNTEwOTMyMw@@._V1_FMjpg_UY2048_.jpg',
-        featured: true,
-    },
-
-];
-
 app.post('/users', (req, res) => {
-    const email = req.body.email;
-    const username = req.body.username;
-    const password = req.body.password;
+    const { email, username, password } = req.body;
     if (email && username && password) {
-        const existingUser = users.find((user) => user.email === email);
-        if (existingUser) {
-            res.status(400).send('This email already exist');
-        } else {
-            users.push({
-                email,
-                username,
-                password,
-                favorite: [],
+        User.findOne({ email })
+            .then(existingUser => {
+                if (existingUser) {
+                    res.status(400).send('This email already exists');
+                } else {
+                    const newUser = new User({
+                        email,
+                        username,
+                        password,
+                        favorite: [],
+                    });
+                    return newUser.save()
+                        .then(() => {
+                            res.status(200).send('User created!');
+                            return;
+                        })
+                }
+            })
+            .catch(error => {
+                console.error('Error creating user:', error);
+                res.status(500).send('Internal server error');
+                return;
             });
-            res.status(200).send('User created!');
-        }
     } else {
-        res.status(500).send('Wrong data');
+        res.status(400).send('Missing data');
     }
 });
+
 
 app.put('/users/:email', (req, res) => {
     const email = req.params.email;
     const username = req.body.username;
     if (username) {
-        const user = users.find((user) => user.email === email);
-        if (!user) {
-            res.status(400).send('This email does not exist');
-        } else {
-            user.username = username;
-            res.status(200).send('User updated!');
-        }
+        User.findOne({ email })
+            .then(user => {
+                if (!user) {
+                    res.status(400).send('This email does not exist');
+                } else {
+                    user.username = username;
+                    return user.save()
+                        .then(() => {
+                            res.status(200).send('User updated!');
+                        })
+                }
+            })
+            .catch(error => {
+                console.error('Error updating user:', error);
+                res.status(500).send('Internal server error');
+            });
     } else {
-        res.status(500).send('Wrong data');
+        res.status(400).send('Missing data');
     }
 });
 
@@ -109,16 +83,27 @@ app.post('/users/:email/:movieName', (req, res) => {
     const email = req.params.email;
     const movieName = req.params.movieName;
     if (email && movieName) {
-        const user = users.find((user) => user.email === email);
-        const movie = movies.find((m) => m.title === movieName);
-        if (!user || !movie) {
-            res.status(400).send('This email or movie does not exist');
-        } else {
-            user.favorite.push(movieName);
-            res.status(200).send('Movie "' + movieName + '" favorited!');
-        }
+        Promise.all([
+            User.findOne({ email }),
+            Movie.findOne({ title: movieName })
+        ])
+            .then(([user, movie]) => {
+                if (!user || !movie) {
+                    res.status(400).send('This email or movie does not exist');
+                } else {
+                    user.movies.push(movie._id);
+                    return user.save()
+                        .then(() => {
+                            res.status(200).send(`Movie "${movieName}" favorited!`);
+                        });
+                }
+            })
+            .catch(error => {
+                console.error('Error favoriting movie:', error);
+                res.status(500).send('Internal server error');
+            });
     } else {
-        res.status(500).send('Wrong data');
+        res.status(400).send('Missing data');
     }
 });
 
@@ -126,55 +111,118 @@ app.delete('/users/:email/:movieName', (req, res) => {
     const email = req.params.email;
     const movieName = req.params.movieName;
     if (email && movieName) {
-        const user = users.find((user) => user.email === email);
-        const movie = movies.find((m) => m.title === movieName);
-        if (!user || !movie) {
-            res.status(400).send('This email or movie does not exist');
-        } else {
-            user.favorite = user.favorite.filter(name => name !== movieName);
-            res.status(200).send('Movie "' + movieName + '" removed from favorite!');
-        }
+        Promise.all([
+            User.findOne({ email }),
+            Movie.findOne({ title: movieName })
+        ])
+            .then(([user, movie]) => {
+                if (!user || !movie) {
+                    res.status(400).send('This email or movie does not exist');
+                } else {
+                    user.favorite = user.movies.filter(id => id !== movie._id);
+                    return user.save()
+                        .then(() => {
+                            res.status(200).send(`Movie "${movieName}" removed from favorites!`);
+                        })
+                }
+            })
+            .catch(error => {
+                console.error('Error removing movie from favorites:', error);
+                res.status(500).send('Internal server error');
+            });
     } else {
-        res.status(500).send('Wrong data');
+        res.status(400).send('Missing data');
     }
 });
 
 app.delete('/users', (req, res) => {
     const email = req.body.email;
     if (email) {
-        const existingUser = users.find((user) => user.email === email);
-        if (!existingUser) {
-            res.status(400).send('This email was not found');
-        } else {
-            users = users.filter(user => user !== existingUser);
-            res.status(200).send('User removed!');
-        }
+        User.findOneAndDelete({ email })
+            .then(deletedUser => {
+                if (!deletedUser) {
+                    res.status(400).send('This email was not found');
+                    return;
+                }
+                res.status(200).send('User removed!');
+            })
+            .catch(error => {
+                console.error('Error removing user:', error);
+                res.status(500).send('Internal server error');
+            });
     } else {
-        res.status(500).send('Wrong data');
+        res.status(400).send('Missing data');
     }
 });
+
 
 app.get('/movies', (req, res) => {
-    res.json(movies);
+    Movie.find()
+        .then(movies => {
+            res.json(movies);
+        })
+        .catch(error => {
+            console.error('Error fetching movies:', error);
+            res.status(500).send('Internal server error');
+        });
 });
 
-app.get('/movies/:name', (req, res) => {
-    const movie = movies.find(movie => movie.title === req.params.name);
-    if (movie) {
-        res.json(movie);
-    } else {
-        res.status(404).send('Movie with the title "' + req.params.name + '" was not found.');
-    }
+app.get('/movies/:title', (req, res) => {
+    const title = req.params.title;
+    Movie.findOne({ title })
+        .then(movie => {
+            if (movie) {
+                res.json(movie);
+            } else {
+                res.status(404).send(`Movie with the title "${title}" was not found.`);
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching movie:', error);
+            res.status(500).send('Internal server error');
+        });
 });
 
 app.get('/movies/genre/:name', (req, res) => {
-    const genreMovies = movies.filter(movie => movie.genre.name === req.params.name);
-    res.json(genreMovies);
+    const genreName = req.params.name;
+    Genre.findOne({ name: genreName })
+        .then(genre => {
+            if (!genre) {
+                res.status(404).send(`Genre "${genreName}" was not found.`);
+                return;
+            }
+            return Movie.find({ 'genre': genre._id });
+        })
+        .then(movies => {
+            if (movies) {
+                res.json(movies);
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching movies by genre:', error);
+            res.status(500).send('Internal server error');
+        });
 });
 
 app.get('/movies/directors/:name', (req, res) => {
-    const directorMovies = movies.filter(movie => movie.director.name === req.params.name);
-    res.json(directorMovies);
+    const directorName = req.params.name;
+    Director.findOne({ name: directorName })
+        .then(director => {
+            if (!director) {
+                res.status(404).send(`Director "${directorName}" was not found.`);
+                return;
+            }
+            return Movie.find({ 'director': director._id });
+        })
+        .then(movies => {
+            if (movies) {
+                res.json(movies);
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching movies by director:', error);
+            res.status(500).send('Internal server error');
+        });
 });
 
 app.get('/', (req, res) => {
---
+++ models.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const genreSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+});
+
+const directorSchema = new mongoose.Schema({
+  name: String,
+  bio: String,
+  birthyear: Date,
+  deathyear: Date,
+});
+
+const movieSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  imageurl: String,
+  featured: Boolean,
+  genre: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Genre',
+  },
+  director: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Director',
+  },
+});
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  email: String,
+  birth_date: Date,
+  movies: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Movie',
+    },
+  ],
+});
+
+const Genre = mongoose.model('Genre', genreSchema);
+const Director = mongoose.model('Director', directorSchema);
+const Movie = mongoose.model('Movie', movieSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports.Genre = Genre;
+module.exports.Director = Director;
+module.exports.Movie = Movie;
+module.exports.User = User;
